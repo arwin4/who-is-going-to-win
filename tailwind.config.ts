53ab406ad4804fa9867b1cfef4ffb1b48972537a
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-07-16 17:31:13 +0200

    Add font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,11 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Noto Sans', 'Arial', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
