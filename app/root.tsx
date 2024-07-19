fb138c6f00275ce5368acdc43ce428cf1aef159a
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-07-19 17:58:03 +0200

    Attempt to fix analytics

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body className="m-0 flex min-h-screen flex-col place-items-center justify-around bg-slate-300  text-gray-800">
-        <Analytics />
         {children}
       </body>
     </html>
@@ -20,5 +19,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+      <Analytics />
+      <Outlet />
+    </>
+  );
 }
