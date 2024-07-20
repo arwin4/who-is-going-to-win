7429c7ac244e232c7c136652764151b3569a9c23
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-07-20 12:11:29 +0200

    Add API endpoint for scrape cron job

---
+++ app/routes/api/listenForCron.ts
@@ -0,0 +1,17 @@
+import type { LoaderFunction } from '@remix-run/node';
+import scrapeAndSave from '~/utils/scrapeAndSave';
+
+export const loader: LoaderFunction = async () => {
+  // Check for a secret key to ensure the request is from your cron service
+  // const authHeader = request.headers.get('Authorization');
+  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //   return json({ error: 'Unauthorized' }, { status: 401 });
+  // }
+
+  try {
+    await scrapeAndSave();
+    console.log('Scraped and saved new data.');
+  } catch (error) {
+    console.error('Scrape failed:', error);
+  }
+};
