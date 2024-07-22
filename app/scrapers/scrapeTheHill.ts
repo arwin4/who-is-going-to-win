2d55e284d147f393306143a21672b6f68675bc1f
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-07-22 16:54:05 +0200

    Use exported loadDoc func

--- app/scrapers/scrapeTheHill.ts
+++ app/scrapers/scrapeTheHill.ts
@@ -1,22 +1,19 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
 import determineResult from './utils/determineResult';
+import loadHtmlForScraping from './utils/loadHtmlForScraping';
 
 export default async function scrapeTheHill() {
-  const response = await axios.get(
-    'https://elections2024.thehill.com/forecast/2024/president/',
+  const url = 'https://elections2024.thehill.com/forecast/2024/president/';
+  const loadedDocument = await loadHtmlForScraping(url);
+
+  const targetElement = loadedDocument(
+    'p:contains("Our model currently predicts that")',
   );
-  const html = response.data;
-
-  const $ = cheerio.load(html);
-
-  const targetElement = $('p:contains("Our model currently predicts that")');
 
   // Extract the complete text content of the element
   const fullPredictionString = targetElement.text();
 
   if (!fullPredictionString) {
-    throw new Error('Could not find the prediction');
+    console.error('Could not find the The Hill prediction');
   }
 
   return determineResult(fullPredictionString);
