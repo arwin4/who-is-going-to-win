985d0c6c115cdf432c2dde5dd19fe808d00bc948
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-07-23 10:12:30 +0200

    Localize outcome determination

--- app/scrapers/scrapeTheEconomist.ts
+++ app/scrapers/scrapeTheEconomist.ts
@@ -1,7 +1,12 @@
 import puppeteer from 'puppeteer';
-import determineResult from './utils/determineResult';
+import { Outcome, Prediction, WinnerPercentage } from './types';
+import { getPercentageFromString } from './utils/getPercentageFromString';
 
-export default async function scrapeTheEconomist() {
+/**
+ * Puppeteer is used because the winner percentage is contained within a graph
+ * that uses JS. (Therefore, we can't just parse the HTML.)
+ */
+export default async function scrapeTheEconomist(): Promise<Prediction> {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
@@ -11,26 +16,65 @@ export default async function scrapeTheEconomist() {
   );
 
   /**
-  *   NOTE: at the time of implementation, this selector contains the winner.
-      It's unknown whether it will still contain the winner if the prediction
-      were to flip. 
-  */
+   *  NOTE: at the time of implementation, this selector contains the winner. It's
+   *  unknown whether it will still contain the winner if the prediction were to
+   *  flip.
+   */
   await page.waitForSelector('.svelte-h0zoai');
 
-  // Extract text from <tspan> elements containing "in 100"
-  const tspanTexts = await page.evaluate(() => {
+  const fullPredictionString = await page.evaluate(() => {
     const tspans = document.querySelectorAll('tspan');
-    return Array.from(tspans)
-      .filter(
-        (tspan) =>
-          tspan.textContent.includes('in 100') &&
-          tspan.textContent.length === 11,
-      )
-      .map((tspan) => tspan.textContent);
+    return Array.from(tspans).find(
+      (tspan) =>
+        tspan.textContent?.includes('in 100') &&
+        tspan.textContent.length === 11,
+    )?.textContent;
   });
   await browser.close();
 
-  const fullPredictionString = tspanTexts[3];
+  if (!fullPredictionString) throw new Error();
 
-  return determineResult(fullPredictionString);
+  let outcome: Outcome;
+
+  const parsedPercentage = getPercentageFromString(fullPredictionString);
+  if (parsedPercentage === 50) {
+    outcome = 'tie';
+    return {
+      outcome,
+      percentage: 50,
+    };
+  }
+
+  // The string may concern the Dem or the Rep, so determine whose it is
+  let candidate: 'democrat' | 'republican';
+  const firstLetterOfPredictionString = fullPredictionString.charAt(0);
+  if (firstLetterOfPredictionString === 'D') {
+    candidate = 'democrat';
+  } else if (firstLetterOfPredictionString === 'R') {
+    candidate = 'republican';
+  } else {
+    throw new Error();
+  }
+
+  let winnerPercentage: WinnerPercentage;
+  if (parsedPercentage < 50) {
+    if (candidate === 'democrat') {
+      outcome = 'republican';
+    } else {
+      outcome = 'democrat';
+    }
+    winnerPercentage = 100 - parsedPercentage;
+  } else {
+    if (candidate === 'democrat') {
+      outcome = 'democrat';
+    } else {
+      outcome = 'republican';
+    }
+    winnerPercentage = parsedPercentage;
+  }
+
+  return {
+    outcome,
+    percentage: winnerPercentage,
+  };
 }
