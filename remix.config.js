faa0263f79dabf563503b30fe668d229bdb2a98c
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-07-28 09:30:51 +0200

    Change remix config

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
   ignoredRouteFiles: ['**/.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  serverModuleFormat: 'cjs',
+  serverModuleFormat: 'esm',
+  // future: {
+  //   v2_errorBoundary: true,
+  //   v2_meta: true,
+  //   v2_normalizeFormMethod: true,
+  //   v2_routeConvention: true,
+  // },
+  // serverBuildPath: 'build/index.js',
 };
