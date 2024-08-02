cd1178f103d6f647116a31f958b226a730351a14
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-08-02 10:04:54 +0200

    Update silver forecast

--- app/routes/explanation.tsx
+++ app/routes/explanation.tsx
@@ -21,7 +21,7 @@ export default function Explanation(): React.JSX.Element {
           prediction is updated manually only{' '}
           <a
             className="underline hover:text-gray-500 hover:decoration-gray-500"
-            href="https://x.com/NateSilver538/status/1818659822452998309"
+            href="https://www.natesilver.net/p/the-presidential-election-is-a-toss"
           >
             when Silver mentions its current prediction publicly
           </a>
