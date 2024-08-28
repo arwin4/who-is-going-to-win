204c40ec6ceed7608b6e2d576758b29b269c17ac
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-08-28 17:26:14 +0200

    Extract prediction string function

--- app/scrapers/scrapeTheEconomist.ts
+++ app/scrapers/scrapeTheEconomist.ts
@@ -2,50 +2,54 @@ import { DemPercentage, RepPercentage, Outcome, Prediction } from './../types';
 import puppeteer from 'puppeteer';
 import { getPercentageFromString } from './utils/getPercentageFromString';
 
+async function getFullPredictionString() {
+  const username = process.env.ECONOMIST_USERNAME as string;
+  const password = process.env.ECONOMIST_PASSWORD as string;
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL.
+  await page.goto(
+    'https://www.economist.com/interactive/us-2024-election/prediction-model/president',
+  );
+
+  await page.waitForSelector('div ::-p-text(Log in)');
+
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('div ::-p-text(Log in)'),
+  ]);
+
+  await page.waitForSelector('#input-6');
+  await page.focus('#input-6');
+  await page.keyboard.type(username);
+  await page.focus('#input-8');
+  await page.keyboard.type(password);
+  await page.keyboard.press('Enter');
+
+  await page.waitForSelector('.svelte-h0zoai');
+
+  const fullPredictionString = await page.evaluate(() => {
+    const tspans = document.querySelectorAll('tspan');
+    return Array.from(tspans).find(
+      (tspan) =>
+        tspan.textContent?.includes('in 100') &&
+        tspan.textContent.length === 11,
+    )?.textContent;
+  });
+  await browser.close();
+
+  return fullPredictionString;
+}
+
 /**
  * Puppeteer is used because the winner percentage is contained within a graph
  * that uses JS. (Therefore, we can't just parse the HTML.)
  */
 export default async function scrapeTheEconomist(): Promise<Prediction> {
-  const username = process.env.ECONOMIST_USERNAME as string;
-  const password = process.env.ECONOMIST_PASSWORD as string;
-  console.log(username);
-  console.log(password);
-
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    // Navigate the page to a URL.
-    await page.goto(
-      'https://www.economist.com/interactive/us-2024-election/prediction-model/president',
-    );
-
-    await page.waitForSelector('div ::-p-text(Log in)');
-
-    await Promise.all([
-      page.waitForNavigation(),
-      page.click('div ::-p-text(Log in)'),
-    ]);
-
-    await page.waitForSelector('#input-6');
-    await page.focus('#input-6');
-    await page.keyboard.type(username);
-    await page.focus('#input-8');
-    await page.keyboard.type(password);
-    await page.keyboard.press('Enter');
-
-    await page.waitForSelector('.svelte-h0zoai');
-
-    const fullPredictionString = await page.evaluate(() => {
-      const tspans = document.querySelectorAll('tspan');
-      return Array.from(tspans).find(
-        (tspan) =>
-          tspan.textContent?.includes('in 100') &&
-          tspan.textContent.length === 11,
-      )?.textContent;
-    });
-    await browser.close();
+    const fullPredictionString = await getFullPredictionString();
 
     console.log('prediction:', fullPredictionString);
 
