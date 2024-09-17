eec204064474c1b6a5b3fa57ffe5c5d14414ba80
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-09-17 10:36:04 +0200

    Re-enable request authorization

--- app/routes/updateTheHill.ts
+++ app/routes/updateTheHill.ts
@@ -11,10 +11,10 @@ const source: Source = 'theHill';
 const scraper: ScrapingFunction = scrapeTheHill;
 
 export const loader = async ({ request }: { request: Request }) => {
-  // const authHeader = request.headers.get('Authorization');
-  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return new Response('Not authorized', { status: 403 });
-  // }
+  const authHeader = request.headers.get('Authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Not authorized', { status: 403 });
+  }
 
   try {
     await updatePrediction(source, scraper);
